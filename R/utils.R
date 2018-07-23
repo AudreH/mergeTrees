32a0d0023e680a9a125faeef071052d689d48636
Julien Chiquet <julien.chiquet@inra.fr>
2018-07-23 18:22:15 +0200

    cleaning, cosmetic, etc.

--- R/utils.R
+++ R/utils.R
@@ -7,27 +7,6 @@ reorder_hc <- function(hc) {
   hc
 }
 
-# reorder_hc_list <- function(hc_list) {
-#
-#   lapply()
-#   # reference: les labels/order of the first tree in the list
-#   ref_label <- hc_list[[1]]$labels
-#
-#   if (!is.null(ref_label)){
-#     hc_list <- lapply(hc_list, FUN = function(hc){
-#       if (!identical(hc$labels, ref_label)) {
-#         new_order <- match(ref_label, hc$labels)
-#         hc$labels <- hc$labels[new_order]
-# ### NOT SURE YET OF THAT PART...
-#         ## hc$order <- hc$order[new_order]
-#         hc$order <- new_order
-#       }
-#       hc
-#     })
-#   }
-#   hc_list
-# }
-
 ##' @export
 as.fusionTree <- function(hc_obj) {
 
