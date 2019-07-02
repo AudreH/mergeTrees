6d77047cbb16e0b8d7934d651f107055d8e36f39
Audrey Hulot <audreyhulot@hotmail.fr>
2019-07-02 10:28:45 +0200

    CRAN version 0.1.1

--- inst/oldies/match_func_int.h
+++
@@ -1,8 +0,0 @@
-#include <Rcpp.h>
-#include <string>
-#include <vector>
-
-# define square(x) ((x)*(x))
-int match_func_int(int x, Rcpp::IntegerVector y) ;
-
-Rcpp::IntegerVector match_func_vectorInt(int x, Rcpp::IntegerVector y);
