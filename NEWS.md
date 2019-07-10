59a349a4614ad0102b14cdeba9b2fb8c6331e54c
Audrey Hulot <audreyhulot@hotmail.fr>
2019-07-10 10:58:09 +0200

    Changes for CRAN

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# Changes in 0.1.3 (07/10 2019)
+
+  -  
+
 # Changes in 0.1.2 (07/03 2019)
 
   - check the labels of the trees so that the function stop if the trees do not have the same leaves labels.
--- R/utils.R
+++ R/utils.R
@@ -7,10 +7,10 @@ reorder_hc <- function(hc) {
   hc
 }
 
-##' Turn an hclust object into an
+##' Turn an hclust object into a fusion path.
 ##'
 ##' @param hc_obj and hclust object
-##' @return a list woth three element: path of fusion, vector of height, ordering.
+##' @return a list with three element: path of fusion, vector of height, ordering.
 as.fusionTree <- function(hc_obj) {
 
   fusionTree <- as_fusionTree(hc_obj$merge, hc_obj$order)
